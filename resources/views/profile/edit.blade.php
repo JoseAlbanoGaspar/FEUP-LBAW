b774ddabc634b87b129127938150ac66574d190d
José Albano Gaspar <up202008561@edu.fe.up.pt>
2022-11-14 18:14:41 +0000

    added username and email to editProfile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,6 +4,22 @@
 <form method="POST" action=""> <!-- ROUTE -->
     {{ csrf_field() }}
 
+    <label for="name">Name</label>
+    <input id="name" type="text" name="name" value="{{ $user->username }}" required autofocus>
+    @if ($errors->has('name'))
+      <span class="error">
+          {{ $errors->first('name') }}
+      </span>
+    @endif
+
+    <label for="email">E-Mail Address</label>
+    <input id="email" type="email" name="email" value="{{ $user->email }}" required>
+    @if ($errors->has('email'))
+      <span class="error">
+          {{ $errors->first('email') }}
+      </span>
+    @endif
+
     <label for="personal_text">Personal Text</label>
     <textarea id="personal_text" type="text" name="personal_text" autofocus>
     {{ $user->personal_text }}
