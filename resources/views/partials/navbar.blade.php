3f8351440421e699b9acb1b9111868708c90babe
DinisSousa2003 <dinis.bessa.sousa@gmail.com>
2022-11-16 15:38:49 +0000

    Responsive padding on navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand navbar-dark bg-dark rounded">
-    <a class="navbar-brand mx-5" href="#">hELP</a>
+    <a class="navbar-brand mx-lg-5 mx-md-4 mx-2" href="#">hELP</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -13,7 +13,7 @@
           <button class="btn btn-primary" type="submit"><i class="fa fa-search"></i></button>
         </form>
         
-        <ul class="navbar-nav mx-5">
+        <ul class="navbar-nav mx-lg-5 mx-md-4 mx-2">
             <li class = "nav-item">
                 <a class="nav-link" href="#">
                   <img src="https://lh3.googleusercontent.com/a/ALm5wu3r7-lOMeLJSo4QbDQnPc30lubqkZ9EraVNBgZ7=k-s48" alt="Dinis Sousa's user avatar" width="24" height="24" class="bar-sm ">
