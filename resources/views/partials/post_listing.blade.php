cfcf45efb80cc796f8911e0eec73295df92a3788
DinisSousa2003 <dinis.bessa.sousa@gmail.com>
2022-11-17 19:00:48 +0000

    Answer card on seperate file

--- resources/views/partials/post_listing.blade.php
+++ resources/views/partials/post_listing.blade.php
@@ -1,45 +1,7 @@
-@php $truncated = (strlen($post_listing->text_body) > 38) ? substr($post_listing->text_body,0,35).'...' :
-    $post_listing->text_body;
-    include_once(app_path() . '/Includes/Utils.php');
-@endphp
-
 @if($post_listing->question)
     @include('partials.question-card', ['post' => $post_listing])
 @elseif($post_listing->answer)
-
-    <div class="answer-card card mx-5 my-2">
-        <div class = "card-body row">
-            <div class="answer-stats col-2">
-
-                <div class="answer-stats-item">
-                    <span class="answer-stats-score-number">{{$post_listing->answer->score}}</span>
-                    <span class="answer-stats-score-unit">votes</span>
-                </div>
-
-                <div class="answer-stats-item">
-                    <span class="answer-stats-is-solution">{{$post_listing->answer->is_solution}}</span>
-                </div>
-            </div>
-
-            <div class="post-summary col-10">
-
-                <div class ="row"><h6 class="original-question-title">Original question:</h6> <a href="{{route('questions', ['id'=>$post_listing->answer->question->id_question])}}"><h7 class="card-title">{{$post_listing->answer->question->title}}</h7></a></div>
-                <div class ="row"><a href="{{route('questions', ['id'=>$post_listing->answer->question->id_question])}}#{{'answerid-'.$post_listing->answer->id_answer}}"><h5 class="card-text">{{$truncated}}</h5></a></div>
-
-
-                @include('partials.profile-card', ['user' => $post_listing->user])
-
-                <time class="user-card-time flex-shrink">
-                    answered {{timeElapsedString($post_listing->date)}}
-                </time>
-
-                </div>
-            </div>
-
-        </div>
-    </div>
-    </div>
-
+    @include('partials.answer-card', ['post' => $post_listing])
 @endif
 
 {{--<article class="post_listing" data-id="{{ $post_listing->id_post }}">--}}
