9549487719476df3298bc6e3fe65e2f2ac7589f9
José Albano Gaspar <up202008561@edu.fe.up.pt>
2022-11-19 17:45:49 +0000

    creating files for adminPage

---
+++ resources/views/pages/admin.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+@section('content')
+<h2>Administration Page</h2>
+@include('partials.search_users',['users' => $users])
+
+@include('partials.tag_management',['tags' => $tags])
+
+
+@endsection
