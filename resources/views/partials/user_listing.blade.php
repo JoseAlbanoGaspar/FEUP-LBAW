7025e7caf5899918873c3de2562c93f9e6e46170
DinisSousa2003 <dinis.bessa.sousa@gmail.com>
2022-11-21 15:42:41 +0000

    Add accordion to admin page. Change on color buttons

--- resources/views/partials/user_listing.blade.php
+++ resources/views/partials/user_listing.blade.php
@@ -15,15 +15,15 @@
    <!--$user = Auth::user()
     if($user->admin()) -->
     <div class="d-flex align-items-center">
-         <a role="button" class="btn btn-secondary btn-sm mx-2 text-center" href="{{route('editProfile',['id_user' => $user_listing->id_user]) }}">Edit</a>
+         <a role="button" class="btn btn-secondary btn-sm mx-2 text-center" href="{{route('editProfile',['id_user' => $user_listing->id_user]) }} ">Edit</a>
 
         <!-- define route (not for A8) -->
-        <a role="button" class="btn btn-secondary btn-sm mx-2 text-center" href="\">Delete</a>
+        <a role="button" class="btn btn-danger btn-sm mx-2 text-center" href="\">Delete</a>
 
         <form method='POST' action="{{route('makeAdmin')}}" class="mx-2 d-flex" >
             {{ csrf_field() }}
             <input type="hidden" value="{{$user_listing->id_user}}" name="id_user"/>
-            <button type="submit" class="btn btn-secondary btn-sm h-auto">Make Admin</button>
+            <button type="submit" class="btn btn-warning btn-sm h-auto">Make Admin</button>
         </form>
     </div>
 </article>
