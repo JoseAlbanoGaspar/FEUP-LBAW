02cf84db75187219f5f56efc99c938ec03cdf6dd
DinisSousa2003 <dinis.bessa.sousa@gmail.com>
2022-11-21 16:00:02 +0000

    Add margin to form so it fits the window

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -4,7 +4,7 @@
 
 <h2 class="m-2 my-4">Administration Page</h2>
 
-<div class="accordion accordion-flush" id="accordionFlushExample">
+<div class="accordion accordion-flush mx-4 mx-md-3" id="accordionFlushExample">
     <div class="accordion-item">
       <h2 class="accordion-header" id="flush-headingOne">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
