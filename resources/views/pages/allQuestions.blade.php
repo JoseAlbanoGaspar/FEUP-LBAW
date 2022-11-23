52c6ca3c8857437b3a1170a626e1055d8ac2c9c3
DinisSousa2003 <dinis.bessa.sousa@gmail.com>
2022-11-23 00:04:44 +0000

    Button add question larger

--- resources/views/pages/allQuestions.blade.php
+++ resources/views/pages/allQuestions.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <div class="d-flex flex-row align-items-center justify-content-between mx-5 my-2">
         <h1 class="my-4"> Questions </h1>
-        <a role="button" class="btn btn-secondary btn-sm mx-2 text-center" href="{{ route('FormToAskQuestion') }}">Ask Question</a>
+        <a role="button" class="btn btn-secondary btn mx-2 text-center" href="{{ route('FormToAskQuestion') }}">Ask Question</a>
     </div>
     @if((count($posts) > 0))
         <section id="search_results">
